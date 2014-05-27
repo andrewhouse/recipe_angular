3f74913723009709c6b0c69bc691f2a5dc622d48
Andrew House <andrewhouse621@gmail.com>
2014-05-27 15:13:36 -0400

    After adding bower/initial angular files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecipeAngular::Application.config.secret_key_base = '8b164a19f592705346904c50b0d4db342581662fe32523c3249168ba8abe94f440ca26bfb29d4811e04ab3ace37133b3b119e712ea7279188585fb5dc0dd5f98'
